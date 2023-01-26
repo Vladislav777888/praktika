32900a316c3ffe0933d948e8e1ee54a84748a147
Vladyslav Solomakha <vladislavsolomaha08@gmail.com>
2023-01-26 19:00:35 +0200

    hngj

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,4 @@
-import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { Layout } from './Layout/Layout';
 import HomePage from 'pages/HomePage';
 
